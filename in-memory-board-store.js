8bf85c0310dc1b546a6218d30302f3a5fd43e69e
Martin Samuelsson <commit@martsam.se>
2014-08-05 15:50:02 +0200

    added get-method to DS, changed add to upsert, listening for put-requests to update items

--- in-memory-board-store.js
+++ in-memory-board-store.js
@@ -7,7 +7,39 @@ exports.getBoard = function() {
   return board;
 };
 
-exports.addItem = function(item) {
-  board.items.push(item);
+exports.upsertItem = function(item) {
+  if (typeof item.id === 'undefined') {
+    item.id = generateUUID();
+    board.items.push(item);
+  } else {
+    for (var i = 0; i < board.items.length; i += 1) {
+      var oldItem = board.items[i];
+      if (oldItem.id === item.id) {
+        board.items[i] = item;
+        break;
+      }
+    }
+  }
+  return item;
 };
 
+exports.getItem = function(itemId) {
+  for (var i = 0; i < board.items.length; i += 1) {
+    var item = board.items[i];
+    if (item.id === itemId) {
+      return item;
+    }
+  }
+};
+
+
+
+var generateUUID = function() {
+  var d = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+  });
+  return uuid;
+};
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-boardStore.addItem({
+boardStore.upsertItem({
   type: 'IMAGE',
   url: 'http://d1.dn-static.se/UploadedImages/2014/8/4/5505ef2c-6bc9-4479-8448-370c50a8cbac/original.jpg',
   position: {
@@ -17,12 +17,12 @@ boardStore.addItem({
   }
 });
 
-boardStore.addItem({
+boardStore.upsertItem({
   type: 'YOUTUBE',
   videoId: 'PTKIEr6V__k'
 });
 
-boardStore.addItem({
+boardStore.upsertItem({
   type: 'SPOTIFY',
   uri: 'spotify:track:6TC8cblDfRetSnRFpJlMdX'
 });
@@ -33,14 +33,14 @@ var server = http.createServer(app);
 var wss = new WebSocketServer({server: server});
 console.log("websocket server created");
 
-wss.on("connection", function (ws) {
+wss.on("connection", function(ws) {
   console.log("websocket connection open");
   ws.on("close", function () {
     console.log("websocket connection closed");
   });
 });
 
-wss.broadcast = function (data) {
+wss.broadcast = function(data) {
   console.log('broadcasting: ' + data);
   for (var i in this.clients) {
     this.clients[i].send(JSON.stringify(data));
@@ -49,14 +49,24 @@ wss.broadcast = function (data) {
 
 var router = express.Router();
 router.route('/board')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.json(boardStore.getBoard());
-  }).post(function (req, res) {
+  }).post(function(req, res) {
     var item = req.body;
-    console.log('adding item:');
-    console.log(item);
-    boardStore.addItem(item);
-    wss.broadcast(item);
+    var newItem = boardStore.upsertItem(item);
+    console.log('added item:');
+    console.log(newItem);
+    wss.broadcast(newItem);
+    res.json({});
+  }).put(function(req, res){
+    var item = req.body;
+    if (typeof item.id !== 'undefined') {
+      var newItem = boardStore.upsertItem(item);
+      console.log('updated item:');
+      console.log(newItem);
+    } else {
+      console.log('item lacks id, refusing to update!');
+    }
     res.json({});
   });
 
